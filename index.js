a8da2a154c8d1d99217115ebf1d5c4b45691cac5
litichevskiy <kunin25@ukr.net>
2018-06-10 23:41:11 +0300

    + fix express server

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, './dist')))
-  .get('/', (req, res) => res.render('./index.html'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use('/dist', express.static(__dirname + '/dist'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen( PORT, () => console.log(`server listening on port ${PORT}`));
